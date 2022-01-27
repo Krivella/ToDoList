bf47e90caba01890edb2bee477c3311e11ad1cd8
Sasha <krivella6@mail.ru>
2022-01-27 21:53:50 +0400

    on the road to end

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ toDoList.elements.taskName.addEventListener('input', function(){
  }else if(taskName.validity.tooLong){
  	this.nextElementSibling.innerText='Количество символов от 3 до 20'
  }else this.nextElementSibling.innerText= ' '
+ taskName.nextElementSibling.innerText.classList.add("message")
  
 });
 
@@ -18,7 +19,7 @@ toDoList.elements.taskDescription.addEventListener('input', function(){
  if (taskDescription.validity.tooShort){
  	this.nextElementSibling.innerText = 'Минимум 5 символов'
  }else this.nextElementSibling.innerText= ' '
- 
+taskDescription.nextElementSibling.innerText.classList.add("message")
 });
 
 
@@ -30,6 +31,7 @@ let inputDateObj = new Date(inputDate);
 	if(inputDateObj.getTime()<today.getTime()){
 		timeForTask.nextElementSibling.innerText = 'Дата не должна быть в прошлом'
 	}else timeForTask.nextElementSibling.innerText= " "
+	timeForTask.nextElementSibling.innerText.classList.add("message")
 	 
 })
 
@@ -41,9 +43,10 @@ if(buttonDiv.children.length<6){
 	let div= document.createElement('div');
 	let newWorker = document.createElement('div');
 	newWorker.innerHTML = `
-	<input id="somePeople" type="text" name="somePeople" placeholder="Добавить участника">`
+	<input id="somePeople" type="text" class="add" name="somePeople" placeholder="Добавить участника">`
 	let removePeople = document.createElement("button")
 	removePeople.innerText = 'X';
+	removePeople.classList.add("remove")
 	div.append(newWorker, removePeople);
 	removePeople.addEventListener("click", function(){
 		div.remove()
@@ -55,6 +58,7 @@ buttonDiv.append(div)
 
 toDoList = document.getElementsByName("toDoList")[0]
 toDoList.addEventListener("submit", function(event){
+	console.log(event.target)
 	event.preventDefault();
 	if(taskName.validity.valid && timeForTask.validity.valid){
 		toDoList.nextElementSibling.innerText = "Задача успешно добавлена"
@@ -63,8 +67,14 @@ toDoList.addEventListener("submit", function(event){
 	let somePeople = document.getElementsByName("somePeople")
 	for (let man of somePeople){
         somePeopls.push(man.value)
-
-let tasks = [];
+//let taskContainer = localStorage.getItem("tasks");
+//if (taskContainer!==undefined){taskContainer = JSON.parse(taskContainer)};
+let last = localStorage.getItem("tasks");
+let tasks;
+if(last) 
+	tasks = JSON.parse(last);
+else
+	tasks = [];
 let storage = {
 	title: taskName.value,
 	description: taskDescription.value,
@@ -73,7 +83,7 @@ let storage = {
 };
 tasks.push(storage);
 
- let systemInt=localStorage.setItem("tasks", JSON.stringify(tasks))
+ localStorage.setItem("tasks", JSON.stringify(tasks))
 
 	}
 	})
--- js/script_1.js
+++ js/script_1.js
@@ -1,33 +1,39 @@
 "use strict"
 let taskContainer = localStorage.getItem("tasks");
+let i = 0;
 taskContainer = JSON.parse(taskContainer);
 for(let item of taskContainer){
 	let columnOfTasks = document.querySelector(".task_container");
 	let oneTask = document.createElement("div");
+	oneTask.setAttribute("data-id", i);
 	oneTask.innerHTML=`
-<div id = "section" class = "container">
-<h2>${item.title}</h2>
-<p>Автор:${item.members}</p>
-</div>
-<div>
-<p>${item.description}</p>
-<p>Выполнить к: </p>
-<p>${item.date}</p>
-</div>`
-columnOfTasks.append(oneTask)
-oneTask.addEventListener("click", function(){
-  if(!(this.classList.contains ( "task_selected"))){
-    this.classList.add("task_selected");
-  }else{
-    this.classList.remove("task_selected");
+		<div id = "section" class = "container">
+		<h2>${item.title}</h2>
+		<p>Автор:${item.members}</p>
+		</div>
+		<div>
+		<p>Что необходимо сделать:${item.description}</p>
+		<p>Выполнить к: </p>
+		<p>${item.date}</p>
+		</div>`;
+	columnOfTasks.append(oneTask)
+	oneTask.addEventListener("click", function(){
+  		if(!(this.classList.contains ( "task_selected"))){
+    		this.classList.add("task_selected");
+  		}else{
+    		this.classList.remove("task_selected");
+		}
+	});
+	i++;
+
 }
-})
+
 let button = document.querySelector(".button_delete");
 button.addEventListener("click", function(){
   let focus=document.querySelectorAll(".task_selected");
   for(let section of focus){
-    let number=section.getElementById("section");;
-    delete taskContainer[number];
+    let number= parseInt(section.getAttribute("data-id"));
+    taskContainer.splice(number, 1);
   }
+  localStorage.setItem("tasks", JSON.stringify(taskContainer))
 })
-}
